1dc19ab5b1229b69a77cda73ae6073dc096fbae5
stephta <stephanieta@hotmail.ca>
2025-03-15 14:43:05 -0700

    reformat code

--- src/app.R
+++ src/app.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(tidyverse)
 library(bslib)
-# note that yukon does not have any age dist data for nurse practitioners
 
 # read in data
 data <- read_csv("data/processed/processed_data.csv") |>
@@ -21,7 +20,9 @@ data <- read_csv("data/processed/processed_data.csv") |>
 ui <- fillPage(
   theme = bslib::bs_theme(bootswatch = "flatly"),
   tags$title("Canadian Primary Care Tracker"),
-  tags$style(HTML("
+  tags$style(
+    HTML(
+      "
   .custom-header {
     padding-top: 20px;
     padding-left: 20px;
@@ -41,71 +42,88 @@ ui <- fillPage(
   .custom-padding-left {
     margin-left: 20px;
   }
-  ")),
-  h1(div("Canadian Primary Care Tracker", class = "custom-header")), 
-  h4(div("Explore the trends and demographics of Canadian primary care workers from 2019-2023!", class = "custom-subtitle")),
+  "
+    )
+  ),
+  h1(div("Canadian Primary Care Tracker", class = "custom-header")),
+  h4(
+    div(
+      "Explore the trends and demographics of Canadian primary care workers from 2019-2023!",
+      class = "custom-subtitle"
+    )
+  ),
   
   
   sidebarLayout(
     sidebarPanel(
-      selectInput("selected_profession_type", "Profession Type", 
-                  choices = c("All",
-                              "Family medicine doctors",
-                              "Nurse practitioners"), 
-                  selected = "All"),
-      selectInput("selected_province_territory", "Province/Territory", 
-                  choices = c("Alberta",
-                              "British Columbia",
-                              "Manitoba",
-                              "New Brunswick",
-                              "Newfoundland and Labrador",
-                              "Northwest Territories",
-                              "Nova Scotia",
-                              "Nunavut",
-                              "Ontario",
-                              "Prince Edward Island",
-                              "Quebec",
-                              "Saskatchewan",
-                              "Yukon"), 
-                  multiple = TRUE),
-      sliderInput("selected_year", "Year", 
-                  min = min(data$year),
-                  max = max(data$year), 
-                  value = c(min(data$year), max(data$year)),
-                  step = 1,
-                  sep =''),
+      selectInput(
+        "selected_profession_type",
+        "Profession Type",
+        choices = c("All", "Family medicine doctors", "Nurse practitioners"),
+        selected = "All"
+      ),
+      selectInput(
+        "selected_province_territory",
+        "Province/Territory",
+        choices = c(
+          "Alberta",
+          "British Columbia",
+          "Manitoba",
+          "New Brunswick",
+          "Newfoundland and Labrador",
+          "Northwest Territories",
+          "Nova Scotia",
+          "Nunavut",
+          "Ontario",
+          "Prince Edward Island",
+          "Quebec",
+          "Saskatchewan",
+          "Yukon"
+        ),
+        multiple = TRUE
+      ),
+      sliderInput(
+        "selected_year",
+        "Year",
+        min = min(data$year),
+        max = max(data$year),
+        value = c(min(data$year), max(data$year)),
+        step = 1,
+        sep = ''
+      ),
       
       width = 3,
       class = "custom-padding-left"
     ),
     
     mainPanel(
-      fluidRow(
-        column(width = 12,
-               card(
-                 card_header("Number of Primary Care Providers Per 100,000 Population Over Time"),
-                 card_body(plotOutput("lineplot", height = "250px")),
-                 full_screen = TRUE, border = TRUE
-               )
+      fluidRow(column(
+        width = 12,
+        card(
+          card_header("Number of Primary Care Providers Per 100,000 Population Over Time"),
+          card_body(plotOutput("lineplot", height = "250px")),
+          full_screen = TRUE,
+          border = TRUE
         )
-      ),
+      )),
       
-      #br(),
-      
-      fluidRow(
-        column(width = 6,
-               card(
-                 card_header("Average Proportion of Primary Care Providers Per Gender"),
-                 card_body(plotOutput("genderplot", height = "250px")),
-                 full_screen = TRUE, border = TRUE
-               )),
-        column(width = 6,
-               card(
-                 card_header("Average Proportion of Primary Care Providers Per Age Group"),
-                 card_body(plotOutput("ageplot", height = "250px")),
-                 full_screen = TRUE, border = TRUE
-               ))
-      ),
+      fluidRow(column(
+        width = 6,
+        card(
+          card_header("Average Proportion of Primary Care Providers Per Gender"),
+          card_body(plotOutput("genderplot", height = "250px")),
+          full_screen = TRUE,
+          border = TRUE
+        )
+      ), column(
+        width = 6,
+        card(
+          card_header("Average Proportion of Primary Care Providers Per Age Group"),
+          card_body(plotOutput("ageplot", height = "250px")),
+          full_screen = TRUE,
+          border = TRUE
+        )
+      )),
       
       fluidRow(),
       
@@ -121,15 +139,16 @@ server <- function(input, output, session) {
     data |>
       # filter profession type
       filter(profession_type == input$selected_profession_type) |>
-      # Filter year range
+      # filter year range
       filter(between(year, input$selected_year[1], input$selected_year[2])) |>
-      # Conditionally filter provinces if any are selected
-      filter(if (length(input$selected_province_territory) > 0) province_territory %in% input$selected_province_territory else TRUE) |>
-      ggplot(aes(x=year, y=count_per_100000, color=province_territory)) +
+      # conditionally filter provinces if any are selected
+      filter(if (length(input$selected_province_territory) > 0)
+        province_territory %in% input$selected_province_territory
+        else
+          TRUE) |>
+      ggplot(aes(x = year, y = count_per_100000, color = province_territory)) +
       geom_line(size = 1) +
-      labs(x = "Year",
-           y = "Number per 100,000 Population",
-           color = "Province/Territory") +
+      labs(x = "Year", y = "Number per 100,000 Population", color = "Province/Territory") +
       theme_minimal() +
       ggthemes::scale_color_gdocs()  +
       theme(text = element_text(size = 12.5))
@@ -139,46 +158,88 @@ server <- function(input, output, session) {
     data |>
       # filter profession type
       filter(profession_type == input$selected_profession_type) |>
-      # Filter year range
+      # filter year range
       filter(between(year, input$selected_year[1], input$selected_year[2])) |>
-      # Conditionally filter provinces if any are selected
-      filter(if (length(input$selected_province_territory) > 0) province_territory %in% input$selected_province_territory else TRUE) |>
+      # conditionally filter provinces if any are selected
+      filter(if (length(input$selected_province_territory) > 0)
+        province_territory %in% input$selected_province_territory
+        else
+          TRUE) |>
       mutate(year = as.factor(year)) |>
       group_by(year) |>
-      summarise(percent_female = mean(percent_female, na.rm = TRUE),
-                percent_male = mean(percent_male, na.rm = TRUE)) |>
-      pivot_longer(cols = c(percent_female, percent_male), 
-                   names_to = "gender", values_to = "percentage") |>
-      mutate(gender = factor(gender, levels = c("percent_male", "percent_female"))) |> 
+      summarise(
+        percent_female = mean(percent_female, na.rm = TRUE),
+        percent_male = mean(percent_male, na.rm = TRUE)
+      ) |>
+      pivot_longer(
+        cols = c(percent_female, percent_male),
+        names_to = "gender",
+        values_to = "percentage"
+      ) |>
+      mutate(gender = factor(gender, levels = c("percent_male", "percent_female"))) |>
       ggplot(aes(x = year, y = percentage, fill = gender)) +
       geom_bar(stat = "identity") +
-      scale_fill_manual(values = c("percent_male" = "#0072B2", "percent_female" = "#E69F00"),
-                        labels = c("percent_male" = "Male", "percent_female" = "Female")) + 
+      scale_fill_manual(
+        values = c(
+          "percent_male" = "#0072B2",
+          "percent_female" = "#E69F00"
+        ),
+        labels = c("percent_male" = "Male", "percent_female" = "Female")
+      ) +
       labs(x = "Year", y = "Percentage", fill = "Gender") +
       theme_minimal() +
       theme(text = element_text(size = 12.5))
   })
-
+  
   output$ageplot <- renderPlot({
     data |>
       # filter profession type
       filter(profession_type == input$selected_profession_type) |>
-      # Filter year range
+      # filter year range
       filter(between(year, input$selected_year[1], input$selected_year[2])) |>
-      # Conditionally filter provinces if any are selected
-      filter(if (length(input$selected_province_territory) > 0) province_territory %in% input$selected_province_territory else TRUE) |>
+      # conditionally filter provinces if any are selected
+      filter(if (length(input$selected_province_territory) > 0)
+        province_territory %in% input$selected_province_territory
+        else
+          TRUE) |>
       mutate(year = as.factor(year)) |>
       group_by(year) |>
-      summarise(percent_age_under_30 = mean(percent_age_under_30, na.rm = TRUE),
-                percent_age_30_to_59 = mean(percent_age_30_to_59, na.rm = TRUE),
-                percent_age_over_60 = mean(percent_age_over_60, na.rm = TRUE)) |>
-      pivot_longer(cols = c(percent_age_under_30, percent_age_30_to_59, percent_age_over_60), 
-                   names_to = "age_group", values_to = "percentage") |>
-      mutate(age_group = factor(age_group, levels = c("percent_age_over_60", "percent_age_30_to_59", "percent_age_under_30"))) |>
+      summarise(
+        percent_age_under_30 = mean(percent_age_under_30, na.rm = TRUE),
+        percent_age_30_to_59 = mean(percent_age_30_to_59, na.rm = TRUE),
+        percent_age_over_60 = mean(percent_age_over_60, na.rm = TRUE)
+      ) |>
+      pivot_longer(
+        cols = c(
+          percent_age_under_30,
+          percent_age_30_to_59,
+          percent_age_over_60
+        ),
+        names_to = "age_group",
+        values_to = "percentage"
+      ) |>
+      mutate(age_group = factor(
+        age_group,
+        levels = c(
+          "percent_age_over_60",
+          "percent_age_30_to_59",
+          "percent_age_under_30"
+        )
+      )) |>
       ggplot(aes(x = year, y = percentage, fill = age_group)) +
       geom_bar(stat = "identity") +
-      scale_fill_manual(values = c("percent_age_under_30" = "#72d660", "percent_age_30_to_59" = "#339ff2", "percent_age_over_60" = "#8b29e6"),
-                        labels = c("percent_age_under_30" = "Percent Under 30 Years Old", "percent_age_30_to_59" = "Percent Between 30 and 59 Years Old", "percent_age_over_60" = "Percent Over 60 Years Old")) + 
+      scale_fill_manual(
+        values = c(
+          "percent_age_under_30" = "#72d660",
+          "percent_age_30_to_59" = "#339ff2",
+          "percent_age_over_60" = "#8b29e6"
+        ),
+        labels = c(
+          "percent_age_under_30" = "Percent Under 30 Years Old",
+          "percent_age_30_to_59" = "Percent Between 30 and 59 Years Old",
+          "percent_age_over_60" = "Percent Over 60 Years Old"
+        )
+      ) +
       labs(x = "Year", y = "Percentage", fill = "Age Group") +
       theme_minimal() +
       theme(text = element_text(size = 12.5))
--- src/process_data.R
+++ src/process_data.R
@@ -13,10 +13,11 @@ library(openxlsx)
 read_save_process_province_territory_data <- function(sheet_number, name) {
   raw_province_territory_data <- read.xlsx(
     "data/raw/raw_data.xlsx",
-    sheet=sheet_number,
-    startRow=2,
-    rows=seq(2, 185),
-    fillMergedCells = TRUE) |>
+    sheet = sheet_number,
+    startRow = 2,
+    rows = seq(2, 185),
+    fillMergedCells = TRUE
+  ) |>
     select(1:8)
   
   processed_province_territory_data <- raw_province_territory_data
@@ -29,17 +30,23 @@ read_save_process_province_territory_data <- function(sheet_number, name) {
     "percent_female",
     "percent_age_under_30",
     "percent_age_30_to_59",
-    "percent_age_over_60") 
+    "percent_age_over_60"
+  )
   
   processed_province_territory_data <- processed_province_territory_data |>
     filter(profession_type == "Family medicine" |
              profession_type == "Nurse practitioners") |>
-    mutate(province_territory = name,
-           count = as.numeric(gsub("[^0-9.]", "", count)),
-           count_per_100000 = as.numeric(gsub("[^0-9.]", "", count_per_100000))
-           ) |>
-    mutate(profession_type = case_when(profession_type == "Family medicine" ~ "Family medicine doctors",
-                                       TRUE ~ profession_type ))
+    mutate(
+      province_territory = name,
+      count = as.numeric(gsub("[^0-9.]", "", count)),
+      count_per_100000 = as.numeric(gsub("[^0-9.]", "", count_per_100000))
+    ) |>
+    mutate(
+      profession_type = case_when(
+        profession_type == "Family medicine" ~ "Family medicine doctors",
+        TRUE ~ profession_type
+      )
+    )
   
   return(processed_province_territory_data)
 }
@@ -48,22 +55,29 @@ main <- function() {
   # download and save raw data
   url <- "https://www.cihi.ca/sites/default/files/document/health-workforce-canada-2019-2023-overview-data-tables-en.xlsx"
   download.file(url, "data/raw/raw_data.xlsx", mode = "wb")
-  raw_data_ <- read_excel("data/raw/raw_data.xlsx", sheet=3, skip=1, n_max=37)
+  raw_data_ <- read_excel(
+    "data/raw/raw_data.xlsx",
+    sheet = 3,
+    skip = 1,
+    n_max = 37
+  )
   
   # put together primary care information for each province/territory
-  names <- c("Newfoundland and Labrador",
-             "Prince Edward Island",
-             "Nova Scotia",
-             "New Brunswick",
-             "Quebec",
-             "Ontario",
-             "Manitoba",
-             "Saskatchewan",
-             "Alberta",
-             "British Columbia",
-             "Yukon",
-             "Northwest Territories",
-             "Nunavut")
+  names <- c(
+    "Newfoundland and Labrador",
+    "Prince Edward Island",
+    "Nova Scotia",
+    "New Brunswick",
+    "Quebec",
+    "Ontario",
+    "Manitoba",
+    "Saskatchewan",
+    "Alberta",
+    "British Columbia",
+    "Yukon",
+    "Northwest Territories",
+    "Nunavut"
+  )
   
   full_data <- tibble(
     profession_type = factor(),
@@ -80,9 +94,8 @@ main <- function() {
   for (i in seq_along(names)) {
     sheet_number = i + 3
     
-    data = read_save_process_province_territory_data(
-      sheet_number=sheet_number,
-      name=names[i])
+    data = read_save_process_province_territory_data(sheet_number = sheet_number, name =
+                                                       names[i])
     
     full_data <- rbind(full_data, data)
   }
@@ -90,22 +103,23 @@ main <- function() {
   # add on population data
   population_data <- read.xlsx(
     "data/raw/raw_data.xlsx",
-    sheet=17,
-    startRow=2,
-    rows=seq(2, 15)) |>
+    sheet = 17,
+    startRow = 2,
+    rows = seq(2, 15)
+  ) |>
     select(1:6)
   
   colnames(population_data) <- c("province_territory", "2019", "2020", "2021", "2022", "2023")
   
   population_data <- population_data |>
-    pivot_longer(c("2019", "2020", "2021", "2022", "2023"),
-                 names_to="year",
-                 values_to = "population") |>
+    pivot_longer(
+      c("2019", "2020", "2021", "2022", "2023"),
+      names_to = "year",
+      values_to = "population"
+    ) |>
     mutate(year = as.numeric(year))
   
-  full_data <- full_join(full_data,
-                         population_data,
-                         by = join_by(year, province_territory))
+  full_data <- full_join(full_data, population_data, by = join_by(year, province_territory))
   
   
   # make summary data
@@ -123,18 +137,36 @@ main <- function() {
     )
   
   summary_data <- full_data |>
-    mutate(count_female_per_100000 = count_per_100000 * percent_female / 100,
-           count_age_under_30_per_100000 = count_per_100000 * percent_age_under_30 / 100,
-           count_age_30_to_59_per_100000 = count_per_100000 * percent_age_30_to_59 / 100,
-           count_age_over_60_per_100000 = count_per_100000 * percent_age_over_60 / 100) |>
+    mutate(
+      count_female_per_100000 = count_per_100000 * percent_female / 100,
+      count_age_under_30_per_100000 = count_per_100000 * percent_age_under_30 / 100,
+      count_age_30_to_59_per_100000 = count_per_100000 * percent_age_30_to_59 / 100,
+      count_age_over_60_per_100000 = count_per_100000 * percent_age_over_60 / 100
+    ) |>
     group_by(year, province_territory) |>
     summarize(
       count = sum(count, na.rm = TRUE),
       count_per_100000 = sum(count_per_100000, na.rm = TRUE),
-      percent_female = ifelse(count_per_100000 == 0, 0, sum(count_female_per_100000, na.rm = TRUE) / count_per_100000 * 100),
-      percent_age_under_30 = ifelse(count_per_100000 == 0, 0, sum(count_age_under_30_per_100000, na.rm = TRUE) / count_per_100000 * 100),
-      percent_age_30_to_59 = ifelse(count_per_100000 == 0, 0, sum(count_age_30_to_59_per_100000, na.rm = TRUE) / count_per_100000 * 100),
-      percent_age_over_60 = ifelse(count_per_100000 == 0, 0, sum(count_age_over_60_per_100000, na.rm = TRUE) / count_per_100000 * 100),
+      percent_female = ifelse(
+        count_per_100000 == 0,
+        0,
+        sum(count_female_per_100000, na.rm = TRUE) / count_per_100000 * 100
+      ),
+      percent_age_under_30 = ifelse(
+        count_per_100000 == 0,
+        0,
+        sum(count_age_under_30_per_100000, na.rm = TRUE) / count_per_100000 * 100
+      ),
+      percent_age_30_to_59 = ifelse(
+        count_per_100000 == 0,
+        0,
+        sum(count_age_30_to_59_per_100000, na.rm = TRUE) / count_per_100000 * 100
+      ),
+      percent_age_over_60 = ifelse(
+        count_per_100000 == 0,
+        0,
+        sum(count_age_over_60_per_100000, na.rm = TRUE) / count_per_100000 * 100
+      ),
       population = mean(population, na.rm = TRUE)
     ) |>
     mutate(profession_type = "All")
@@ -151,4 +183,3 @@ main <- function() {
 
 source("src/process_data.R")
 main()
-
